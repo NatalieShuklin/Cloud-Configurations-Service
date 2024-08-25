86598793eda092703c5ceb07090f917835b61c8d
NatalieShuklin <natalieshuklin@gmail.com>
2024-08-25 11:12:13 +0300

    Update GitHubRequiredConfigProvider.py

--- GitHubRequiredConfigProvider.py
+++ GitHubRequiredConfigProvider.py
@@ -1,5 +1,11 @@
 # GitHub Required Config Provider
 class GitHubRequiredConfigProvider:
+
+memebers:
+maps of: - most_strict_security_configs
+- configs_by_role
+- less_strict_security_configs
+    
     def get_required_security_configs(self):
         """Returns GitHub's required security configurations."""
-        return {"BranchProtection": True, "branch_protection": True}
+        return most_strict_security_configs -> #{"BranchProtection": True, "branch_protection": True}
